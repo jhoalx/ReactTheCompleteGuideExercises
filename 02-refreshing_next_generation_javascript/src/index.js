fb0b43c9ee23b90fa54773ab92836b53efb2e2c3
John Alejandro Zapata <jhoalx@gmail.com>
2020-03-25 15:59:52 -0500

    Lecture 13 - Understanding "let" and "const"

--- 02-refreshing_next_generation_javascript/src/index.js
+++ 02-refreshing_next_generation_javascript/src/index.js
@@ -6,7 +6,7 @@ class Section02 extends React.Component {
   render() {
     return (
       <div>
-        
+        <h1>Checkout Console</h1>
       </div>
     );
   }
@@ -16,3 +16,25 @@ ReactDOM.render(
   <Section02 />,
   document.getElementById('root')
 );
+
+//#############################################################################
+
+// Lecture 13 - Understanding "let" and "const"
+
+/* Variables declared by var keyword are scoped to the
+Immediate function body (hence the function scope) */
+var myName = "John";
+console.log(myName);
+
+
+/* let variables are scoped to the immediate enclosing
+block denoted by { } (hence the block scope). */
+let mySecondName = "Alejandro";
+console.log(mySecondName);
+
+
+// const for Constant
+const myLastName = "Zapata";
+//myLastName = "Roldan"; // Uncaught TypeError: Assignment to constant variable
+//previous line stops script execution if uncommented
+console.log(myLastName); //execution wont make it here

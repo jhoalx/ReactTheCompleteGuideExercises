e03d3ac661e7f046e6a8b3d95ff7c21ad4a5c7e7
John Alejandro Zapata <jhoalx@gmail.com>
2020-03-25 14:52:22 -0500

    Playground setup

---
+++ 02-refreshing_next_generation_javascript/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Section02 extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Section02 />,
+  document.getElementById('root')
+);

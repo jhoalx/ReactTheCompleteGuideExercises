d6cb5c155ad8f571d04c0f2ede5eb987414f1ccc
John Alejandro Zapata <jhoalx@gmail.com>
2020-03-26 16:26:48 -0500

    Playground setup

---
+++ 03-understanding_the_base_features_and_syntax/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Section03 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Check Out The Console</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Section03 />,
+  document.getElementById('root')
+);
+
+//#############################################################################
+
+// Lecture 25 - Module Introduction

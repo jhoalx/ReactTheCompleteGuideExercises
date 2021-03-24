1912d10613819080e057f5d5f336b9f3a3232679
John Alejandro Zapata <jhoalx@gmail.com>
2021-03-24 16:04:29 -0500

    Lecture 42 - Manipulating the State

--- 03-understanding_the_base_features_and_syntax/src/index.js
+++ 03-understanding_the_base_features_and_syntax/src/index.js
@@ -14,7 +14,15 @@ class Section03 extends React.Component {
   }
 
   switchNameHandler = () => {
-    console.log("Clicked");
+    // Lecture 42 - Manipulating the State
+    // this.state.persons[0].name = "Alejandro"; //DO NOT DO THIS
+    this.setState({
+      persons: [
+        {name: "Alejandro", age: 34},
+        {name: "Laura", age: 14},
+        {name: "Blanca", age: 56}
+      ]
+    })
   }
 
   render() {
@@ -29,7 +37,7 @@ class Section03 extends React.Component {
 
           {/* Lecture 41 - To which Events Can You Listen:
            https://reactjs.org/docs/events.html#supported-events */}
-          
+
 
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> {/* self closing, no need to nest elements right now */}
           {/* Lecture 34 - Working with Components & Re-Using them */}

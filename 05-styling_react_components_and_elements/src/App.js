73532230dade72e0579a0c92b7c098221cf71382
John Alejandro Zapata <jhoalx@gmail.com>
2021-03-31 09:50:23 -0500

    Playground setup

---
+++ 05-styling_react_components_and_elements/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+class Section05 extends React.Component {
+  render() {
+    return (
+        <div className="App">
+          <p>Playground setup</p>
+        </div>
+    );
+  }
+}
+
+
+export default Section05;

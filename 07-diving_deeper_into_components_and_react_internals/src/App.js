09140b845a37a8c54defc907b1951ca34cf74023
John Alejandro Zapata <jhoalx@gmail.com>
2021-04-05 11:56:52 -0500

    Playground setup

---
+++ 07-diving_deeper_into_components_and_react_internals/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class Section07 extends React.Component{
+  render() {
+    return (
+        <div className="App">
+          <h1>Playground setup</h1>
+        </div>
+    )
+  };
+}
+
+export default Section07;
